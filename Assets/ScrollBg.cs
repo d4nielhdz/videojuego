f43a9a11bc4699c0c116da83280bb3b73da33664
Jose Daniel Hernandez Rios <a00825596@itesm.mx>
2020-09-21 20:03:43 -0500

    Added scrolling background

---
+++ Assets/ScrollBg.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBg : MonoBehaviour {
+    // Start is called before the first frame update
+    public float speed = 0.5f;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float horizontal = Input.GetAxis ("Horizontal");
+        
+        if (Mathf.Approximately(horizontal, 1.0f)) {
+            Vector2 offset = new Vector2 (Time.time * horizontal * speed, 0);
+        GetComponent<Renderer> ().material.mainTextureOffset = offset;
+        }
+    }
+}
