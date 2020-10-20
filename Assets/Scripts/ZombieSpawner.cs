6449ae0560d23e39143f4052aecff78c7c8921f1
Jose Daniel Hernandez Rios <a00825596@itesm.mx>
2020-10-19 19:28:31 -0500

    Created zombie prefab and added spawner script

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+    public GameObject enemy;
+    public int xPos, yPos, enemyCount;
+
+    // Start is called before the first frame update
+    void Start () {
+        enemyCount = 0;
+        StartCoroutine (ZombieSpawn ());
+    }
+
+    IEnumerator ZombieSpawn () {
+
+        while (true) {
+            if (enemyCount < 10) {
+                xPos = Random.Range (-6, 2);
+                yPos = Random.Range (-6, 2);
+
+                Instantiate (enemy, new Vector3 (xPos, yPos, 0), Quaternion.identity);
+
+                enemyCount++;
+            }
+        }
+
+        yield return new WaitForSeconds (1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/ZombieController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public float speed;
+    public float changeTime = 3.0f;
+    int direction = 1;
+    float timer;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0) {
+            direction = -direction;
+        }
+
+        Vector2 position = rigidBody.position;
+        position.x = position.x + direction * speed * Time.deltaTime;
+        rigidBody.MovePosition(position);
+
+    }
+}
