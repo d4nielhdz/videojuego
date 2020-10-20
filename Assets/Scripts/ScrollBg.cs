2527619668c0f80c57391789a18cda6b8152c959
Jose Daniel Hernandez Rios <a00825596@itesm.mx>
2020-10-19 20:22:20 -0500

    Player is only animated when it moves

--- Assets/Scripts/ScrollBg.cs
+++ Assets/Scripts/ScrollBg.cs
@@ -6,9 +6,11 @@ public class ScrollBg : MonoBehaviour {
     // Start is called before the first frame update
     public float speed = 0.5f;
     public bool isMoving;
+    private Animator playerAnim;
     float myTime = 0.0f;
-    void Start () {
 
+    void Start () {
+        playerAnim = GameObject.Find ("Gunner").GetComponent<Animator> ();
     }
 
     // Update is called once per frame
@@ -16,18 +18,14 @@ public class ScrollBg : MonoBehaviour {
         float horizontal = Input.GetAxis ("Horizontal");
         if (Input.GetKey (KeyCode.RightArrow)) {
             isMoving = true;
+            playerAnim.enabled = true;
             myTime += 0.1f;
             Vector2 offset = new Vector2 (myTime * horizontal * speed, 0);
             GetComponent<Renderer> ().material.mainTextureOffset = offset;
-        }
-        //  else if (Input.GetKey (KeyCode.LeftArrow)) {
-        //     isMoving = true;
-        //     myTime += 0.1f;
-        //     Vector2 offset = new Vector2 (myTime * horizontal * speed * -1, 0);
-        //     GetComponent<Renderer> ().material.mainTextureOffset = offset;
-
-        // } 
-        else {
+        } else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.UpArrow)) {
+            playerAnim.enabled = true;
+        } else {
+            playerAnim.enabled = false;
             isMoving = false;
         }
     }
