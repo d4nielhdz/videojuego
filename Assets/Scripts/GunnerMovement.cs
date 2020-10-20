91fcbe80a1dc923ed26d598d705d55fb4cc80a1f
Jose Daniel Hernandez Rios <a00825596@itesm.mx>
2020-10-19 21:42:18 -0500

    Change gunner's animator controller based on state

--- Assets/Scripts/GunnerMovement.cs
+++ Assets/Scripts/GunnerMovement.cs
@@ -16,38 +16,13 @@ public class GunnerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        // // Al presionar W quiero incrementar Y
-        // if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        // {
-        //     //this.transform.position = new Vector3(transform.position.x, transform.position.y + 1, 0);
-        //     transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
-        // }
-        // // Al presionar S quiero decrementar Y
-        // if (Input.GetKey(KeyCode.S))
-        // {
-        //     // this.transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
-        //     transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
-        // }
-        // // Al presionar D quiero incrementar X
-        // if (Input.GetKey(KeyCode.D))
-        // {
-        //     // this.transform.position = new Vector3(transform.position.x + 1, transform.position.y, 0);
-        //     transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
-        // }
-        // // Al presionar A quiero decrementar X
-        // if (Input.GetKey(KeyCode.A))
-        // {
-        //     // this.transform.position = new Vector3(transform.position.x - 1, transform.position.y, 0);
-        //     transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
-
-        // }
         float yPos = transform.position.y;
         float translation = Input.GetAxis ("Vertical");
         float lowerLimit = 4.07f;
         float upperLimit = 9.90f;
 
-        if (Input.GetKey(KeyCode.Space)) {
-            audioData.Play(0);
+        if (Input.GetKey (KeyCode.Space)) {
+            audioData.Play (0);
         }
 
         if (yPos <= upperLimit && yPos >= lowerLimit) {
@@ -60,3 +35,28 @@ public class GunnerMovement : MonoBehaviour {
     }
 
 }
+// // Al presionar W quiero incrementar Y
+// if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+// {
+//     //this.transform.position = new Vector3(transform.position.x, transform.position.y + 1, 0);
+//     transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+// }
+// // Al presionar S quiero decrementar Y
+// if (Input.GetKey(KeyCode.S))
+// {
+//     // this.transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
+//     transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
+// }
+// // Al presionar D quiero incrementar X
+// if (Input.GetKey(KeyCode.D))
+// {
+//     // this.transform.position = new Vector3(transform.position.x + 1, transform.position.y, 0);
+//     transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+// }
+// // Al presionar A quiero decrementar X
+// if (Input.GetKey(KeyCode.A))
+// {
+//     // this.transform.position = new Vector3(transform.position.x - 1, transform.position.y, 0);
+//     transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+
+// }
--- Assets/Scripts/ScrollBg.cs
+++ Assets/Scripts/ScrollBg.cs
@@ -7,10 +7,12 @@ public class ScrollBg : MonoBehaviour {
     public float speed = 0.5f;
     public bool isMoving;
     private Animator playerAnim;
+    private int idleHash;
     float myTime = 0.0f;
 
     void Start () {
         playerAnim = GameObject.Find ("Gunner").GetComponent<Animator> ();
+        idleHash = Animator.StringToHash ("IdleGunner");
     }
 
     // Update is called once per frame
@@ -18,14 +20,14 @@ public class ScrollBg : MonoBehaviour {
         float horizontal = Input.GetAxis ("Horizontal");
         if (Input.GetKey (KeyCode.RightArrow)) {
             isMoving = true;
-            playerAnim.enabled = true;
+            playerAnim.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController> ("BlackGunnerRun");
             myTime += 0.1f;
             Vector2 offset = new Vector2 (myTime * horizontal * speed, 0);
             GetComponent<Renderer> ().material.mainTextureOffset = offset;
         } else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.UpArrow)) {
-            playerAnim.enabled = true;
+            playerAnim.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController> ("BlackGunnerRun");
         } else {
-            playerAnim.enabled = false;
+            playerAnim.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController> ("BlackGunnerIdle");
             isMoving = false;
         }
     }
