6bd6c212d3cbdceb5e2000e09505f3f61b5ac291
Jose Daniel Hernandez Rios <a00825596@itesm.mx>
2020-11-09 20:42:30 -0600

    Add .gitignore

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -3,30 +3,34 @@
 
  public class ZombieMovement : MonoBehaviour {
      public GameObject Background;
+     public GameObject particles;
+     private GameObject e;
      private Vector3 pos;
 
      // Use this for initialization
      void Start () {
-         Background = GameObject.Find("Background");
+         Background = GameObject.Find ("Background");
      }
 
      // Update is called once per frame
      void Update () {
          ScrollBg scrollScript = Background.GetComponent<ScrollBg> ();
-         pos = GetComponent<Transform>().position;
+         pos = GetComponent<Transform> ().position;
          if (scrollScript.isMoving) {
-            GetComponent<Rigidbody2D> ().transform.Translate (-0.1f, 0, 0);
+             GetComponent<Rigidbody2D> ().transform.Translate (-0.1f, 0, 0);
          }
          if (pos.x < 5.3f) {
-             Destroy(gameObject);
+             Destroy (gameObject);
          }
      }
 
-     void OnTriggerEnter2D(Collider2D other) {
-         Debug.Log("collision");
+     void OnTriggerEnter2D (Collider2D other) {
          if (other.tag == "Gunner") {
-             Destroy(other.gameObject);
+             e = Instantiate (particles, new Vector3 (other.transform.position.x, other.transform.position.y, 2), Quaternion.identity);
+            //  e.GetComponent<Renderer>().gi
+             particles.transform.position = other.transform.position;
+             Destroy (other.gameObject);
          }
      }
-     
+
  }
